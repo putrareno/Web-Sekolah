4161275c1f28e466c2d4531e0e9b587b33dafac0
Palaguna <palaguna1501@gmail.com>
2026-09-25 18:40:35 +0700

    ATMINT MEMBERI UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from "../Components/navbar";
 import Hero from "../Components/hero";
 import About from "../Components/about";
 import Footer from "../Components/footer";
+import Eskul from "../Components/ekstrakurikuler";
+import Akademik from "../Components/akademik";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Eskul />
+      <Akademik />
       <Footer />
     </>
   );
