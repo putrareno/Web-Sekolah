961b1f2555922ea95b6c8c57854d25940aea5287
putrareno <ptraarenoo@gmail.com>
2026-09-25 18:11:56 +0700

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Navbar from "../Components/navbar";
-import Hero from "../Components/hero";
-import Footer from "../Components/footer";
-import About from "../Components/about";
+import Navbar from "./Components/navbar";
+import Hero from "./Components/hero";
+import About from "./Components/about";
+import Footer from "./Components/footer";
 
 function App() {
   return (
