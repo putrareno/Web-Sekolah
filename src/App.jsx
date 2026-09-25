117e435ee30d3dd638670e6e6e500c000f8824df
putrareno <ptraarenoo@gmail.com>
2026-09-25 18:15:42 +0700

    tambahin titik

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Navbar from "./Components/navbar";
-import Hero from "./Components/hero";
-import About from "./Components/about";
-import Footer from "./Components/footer";
+import Navbar from "../Components/navbar";
+import Hero from "../Components/hero";
+import About from "../Components/about";
+import Footer from "../Components/footer";
 
 function App() {
   return (
