5ec561055b5bb04936c1851aaefe0f9a91970af6
Palaguna <palaguna1501@gmail.com>
2026-09-25 17:08:33 +0700

    Update Dari Admind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Navbar from "./Components/navbar";
-import Hero from "./Components/hero";
-import Footer from "./Components/footer";
+import Navbar from "../Components/navbar";
+import Hero from "../Components/hero";
+import Footer from "../Components/footer";
 
 function App() {
   return (
